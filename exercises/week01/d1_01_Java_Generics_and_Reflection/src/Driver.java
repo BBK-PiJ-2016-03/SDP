ff91256f33bd75839b8fb45bfd0f4abcbf3352ea
Alexander Worton <aworto01@mail.bbk.ac.uk>
2017-01-10 14:55:13 +0000

    Fiddled with intellij

---
+++ exercises/week01/d1_01_Java_Generics_and_Reflection/src/Driver.java
@@ -0,0 +1,46 @@
+/**
+ * Created by Alexander Worton on 10/01/2017.
+ */
+public class Driver {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        Storage<BankAccount> aStorage = new Storage<>();
+        Storage<String> sStorage = new Storage<>();
+
+        /*
+        1. What are the reasons for using generics here?
+            To allow the Storage class to hold values of any type
+        2. What are the benefits?
+            Not having to create a separate Storage class for each type you wish to hold
+            Not having to use Object type and losing access to methods for the stored type
+                Not having to use reflection to access available methods or make a check against the type of object held
+        */
+
+        Class<BankAccount> baCls = BankAccount.class;
+        try {
+            BankAccount myAccount = baCls.newInstance();
+            aStorage.setValue(myAccount);
+            // Deposit
+            myAccount.deposit(15);
+        }
+        catch ( InstantiationException e ) {
+            // ...
+        }
+        catch ( IllegalAccessException e ) {
+            // ...
+        }
+
+        //problem is that myAccount needs to be cast to BankAccount
+        //Simply changing the type of the object to BankAccount isn't enough as an explicit cast is required
+        //explicit cast is not safe, as not guaranteed that the object is of that type.
+        //changing the Class to the generic Class<BankAccount> removes the need for the explicit cast.
+
+        System.out.println( aStorage.getValue().showBalance() );
+        if( aStorage.getClass() == sStorage.getClass() ) {
+            System.out.println( "EQUAL" );
+        } else {
+            System.out.println( "NOT EQUAL" );
+        }
+    }
+}
+
