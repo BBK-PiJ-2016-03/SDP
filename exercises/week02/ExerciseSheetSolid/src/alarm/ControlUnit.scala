0e6d46ba367a2287fcd5ed9789f927a1e614cf24
Alexander Worton <kesomir@btinternet.com>
2017-01-17 17:43:45 +0000

    refactored ControlUnit

--- exercises/week02/ExerciseSheetSolid/src/alarm/ControlUnit.scala
+++ exercises/week02/ExerciseSheetSolid/src/alarm/ControlUnit.scala
@@ -9,21 +9,22 @@ object ControlUnit {
     println("Battery: " + sensor.getBatteryPercentage)
     println("Type: " + sensor.getSensorType)
     println("Location: " + sensor.getLocation)
+    println("Battery drain per poll: " + checkBatteryDrain(sensor) + "%")
+    printf("Triggers: %.2f%%",checkTriggerPercentage(sensor))
+  }
 
+  def checkTriggerPercentage(sensor: Sensor): Double = {
+    val range = 1 to 100000
+    val triggers: Double = range.count(x => sensor.isTriggered)
+    triggers / 1000
+  }
+
+  def checkBatteryDrain(sensor: Sensor): Double = {
     var i: Int = 0
     do {
       sensor.isTriggered
       i += 1
-    }while(sensor.getBatteryPercentage > 0)
-
-    println("Battery drain per poll: " + (100 / i) + "%")
-
-    val range = 1 to 100000
-
-    val triggers: Double = range.count(x => sensor.isTriggered)
-    val triggerPercentage: Double = triggers / 1000
-
-    println("Triggers: " + triggerPercentage + "%")
-
+    } while(sensor.getBatteryPercentage > 0)
+    100 / i
   }
 }
