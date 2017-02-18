76a34f85b1ebfae031fce789570bcd957057e5d9
Alexander Worton <kesomir@btinternet.com>
2017-02-18 18:02:32 +0000

    Implemented 3c and cleared out a lot of the methods

--- exercises/week05/src/main/scala/films/Director.scala
+++ exercises/week05/src/main/scala/films/Director.scala
@@ -2,19 +2,10 @@ package films
 /**
   * Created by aworton on 18/02/17.
   */
-class Director(val firstName: String, val lastName: String, val yearOfBirth: Int){
+case class Director(firstName: String, lastName: String, yearOfBirth: Int){
   def name = firstName+" "+lastName
-
-  override def equals(obj: Any) = obj match{
-    case obj: Director => obj.firstName == this.firstName &&
-      obj.lastName == this.lastName &&
-      obj.yearOfBirth == this.yearOfBirth
-    case _ => false
-  }
 }
 
 object Director{
-  def apply(firstName: String, lastName: String, yearOfBirth: Int): Director = new Director(firstName, lastName, yearOfBirth)
-
   def older(director1: Director, director2: Director): Director = if(director1.yearOfBirth < director2.yearOfBirth) director1 else director2
 }
--- exercises/week05/src/main/scala/films/Film.scala
+++ exercises/week05/src/main/scala/films/Film.scala
@@ -3,30 +3,13 @@ package films
 /**
   * Created by aworton on 18/02/17.
   */
-class Film(val name: String, val yearOfRelease: Int, val imdbRating: Double, val director: Director) {
+case class Film(name: String, yearOfRelease: Int, imdbRating: Double, director: Director) {
   def directorsAge = yearOfRelease - director.yearOfBirth
 
   def isDirectedBy(director: Director) = director == this.director
-
-  def copy(name: String = this.name,
-           yearOfRelease: Int = this.yearOfRelease,
-           imdbRating: Double = this.imdbRating,
-           director: Director = this.director) = {
-    new Film(name, yearOfRelease, imdbRating, director)
-  }
-
-  override def equals(obj: Any) = obj match{
-    case obj: Film => obj.name == this.name &&
-      obj.yearOfRelease == this.yearOfRelease &&
-      obj.imdbRating == this.imdbRating &&
-      obj.director == this.director
-    case _ => false
-  }
 }
 
 object Film{
-
-  def apply(name: String, yearOfRelease: Int, imdbRating: Double, director: Director): Film = new Film(name, yearOfRelease, imdbRating, director)
 
   def highestRating(film1: Film, film2: Film): Double = if(film1.imdbRating > film2.imdbRating) film1.imdbRating else film2.imdbRating
 
