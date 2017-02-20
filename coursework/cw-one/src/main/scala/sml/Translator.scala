0970d3dca848bb533173806b2ef6919e1bd9a174
Alexander Worton <kesomir@btinternet.com>
2017-02-20 15:05:30 +0000

    Finally got reflection working in translator.

--- coursework/cw-one/src/main/scala/sml/Translator.scala
+++ coursework/cw-one/src/main/scala/sml/Translator.scala
@@ -1,15 +1,11 @@
 package sml
 
+import scala.io.Source
+
 /*
  * The translator of a <b>S</b><b>M</b>al<b>L</b> program.
  */
 class Translator(fileName: String) {
-  private final val ADD = "add"
-  private final val LIN = "lin"
-  private final val BNZ = "bnz"
-  private final val MUL = "mul"
-  private final val SUB = "sub"
-  private final val OUT = "out"
 
   /**
     * translate the small program in the file into lab (the labels) and prog (the program)
@@ -17,39 +13,92 @@ class Translator(fileName: String) {
   def readAndTranslate(m: Machine): Machine = {
     val labels = m.labels
     var program = m.prog
-    import scala.io.Source
-    import scala.reflect.runtime.universe._
-    val lines = Source.fromFile(fileName).getLines
-    for (line <- lines) {
+
+    val lines = Source.fromFile(fileName).getLines.foreach(line => {
       val fields = line.split(" ")
-      if (fields.length > 0) {
-        labels.add(fields(0))
+      addLabel(fields, labels)
 
-        //locate class matching fields(1)
-        //val instructionType = typeOf[Instruction].members.find(symbol => symbol.name.toString.indexOf(fields(1).capitalize) == 0)
-        val instructionType = typeOf[Instruction].members.filter(symbol => {
-          //println("Name: "+symbol.name.toString)
-          symbol.name.toString.indexOf(fields(1).capitalize) == 0
-        })
+      val arguments: Array[AnyRef] = getArgumentsFromFields(fields)
+      val instructionInstance = instantiateInstructionClass(getInstructionName(fields), arguments)
 
-        println("BaseClasses")
-        //typeOf[Instruction].
-
-        //println(instructionType)
-
-        if(instructionType.isEmpty){
-          println(s"Unknown instruction ${fields(1)}")
-        }
-        else{
-          //instantiate
-          program = program :+ instructionType.getClass.getConstructors()(0).newInstance(fields).asInstanceOf[Instruction]
-        }
-
-
+      if(instructionInstance != None) {
+        program = addInstructionToProgram(instructionInstance.get, program, getInstructionName(fields))
       }
-    }
+      else{
+        println("Instruction " + getInstructionName(fields) + " not recognised")
+      }
+    })
+
     new Machine(labels, program)
   }
+
+  private def getInstructionName(fields: Array[String]): String = fields(1)
+
+  /**
+    * add the label to the labels collection
+    * @param fields the array of strings holding the fields
+    * @param labels the labels component of the machine
+    */
+  private def addLabel(fields: Array[String], labels: Labels): Labels = {
+    if (fields.length > 0){
+      labels.add(fields(0))
+    }
+    labels
+  }
+
+  private def getArgumentsFromFields(fields: Array[String]): Array[AnyRef] = {
+    if(fields.length > 2) {
+      ((fields.splitAt(2)_1) ++ ((fields.splitAt(2)_2)).map(argument => argument.toInt)).asInstanceOf[Array[AnyRef]]
+    }
+    else {
+      Array()
+    }
+  }
+
+  /**
+    * instantiate and return the instantiated class matching the instruction as an option
+    * return none if the class could not be instantiated
+    * @param arguments the arguments for the constructor
+    * @return an option containing the instantiated class
+    */
+  private def instantiateInstructionClass(instructionName: String, arguments: Array[AnyRef]): Option[Instruction] = {
+    try {
+      val instructionType = Class.forName(getInstructionClassName(instructionName))
+      Some(instructionType.getConstructors()(0).newInstance(arguments:_*).asInstanceOf[Instruction])
+    }
+    catch{
+      case e: ClassNotFoundException => None
+      case e: IllegalArgumentException => {
+        println("Wrong number of arguments: ")
+        arguments.foreach(arg => println(arg))
+        None
+      }
+    }
+  }
+
+  /**
+    * add the known instruction to the program
+    * @param instructionInstance the instantiated instruction class
+    * @param program the existing Vector of instructions
+    * @param instruction the name of the instruction being added
+    */
+  private def addInstructionToProgram(instructionInstance: Instruction, program: Vector[Instruction], instruction: String): Vector[Instruction] = {
+    if(instructionInstance == None){
+      println(s"Unknown instruction ${instruction}")
+      program
+    }
+    else{
+      val updatedProgram = program :+ instructionInstance
+      updatedProgram
+    }
+  }
+
+  /**
+    * get the qualified class name for the instruction
+    * @param instructionHandle the identifier for the instruction
+    * @return the qualified class name
+    */
+  private def getInstructionClassName(instructionHandle: String): String = "sml." + instructionHandle.capitalize + "Instruction"
 }
 
 object Translator {
