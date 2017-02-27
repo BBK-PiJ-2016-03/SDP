955c54c88561b0bc5dc220da35df1fdd05b3de30
Alexander Worton <kesomir@btinternet.com>
2017-02-27 11:57:38 +0000

    Implemented adaptor pattern

--- exercises/week07/src/main/scala/xpay/payDAdaptor.scala
+++ exercises/week07/src/main/scala/xpay/payDAdaptor.scala
@@ -4,6 +4,7 @@ package xpay
   * Created by aworton on 21/02/17.
   */
 class payDAdaptor(instance: PayD) extends Xpay{
+
   override def getCreditCardNo: String = instance.getCustCardNo
 
   override def setCreditCardNo(creditCardNo: String): Unit = instance.setCustCardNo(creditCardNo)
@@ -12,19 +13,28 @@ class payDAdaptor(instance: PayD) extends Xpay{
 
   override def setCustomerName(customerName: String): Unit = instance.setCardOwnerName(customerName)
 
-  override def getCardExpMonth: String = instance.getCardExpMonthDate
+  override def getCardExpMonth: String = getMonthFromMonthDate(instance.getCardExpMonthDate)
 
-  override def setCardExpMonth(cardExpMonth: String): Unit = instance.setCardExpMonthDate(cardExpMonth)
+  override def setCardExpMonth(cardExpMonth: String): Unit = {
+    val newYear = getYearFromMonthDate(instance.getCardExpMonthDate)
+    instance.setCardExpMonthDate(cardExpMonth + "/" + newYear)
+  }
 
-  override def getCardExpYear: String = instance.
+  override def getCardExpYear: String = getYearFromMonthDate(instance.getCardExpMonthDate)
 
-  override def setCardExpYear(cardExpYear: String): Unit = ???
+  override def setCardExpYear(cardExpYear: String): Unit = {
+    val newMonth = getMonthFromMonthDate(instance.getCardExpMonthDate)
+    instance.setCardExpMonthDate(newMonth + "/" + cardExpYear)
+  }
 
-  override def getCardCVVNo: Short = instance.getCVVNo
+  override def getCardCVVNo: Short = instance.getCVVNo.toShort
 
-  override def setCardCVVNo(cardCVVNo: Short): Unit = ???
+  override def setCardCVVNo(cardCVVNo: Short): Unit = instance.setCVVNo(cardCVVNo.toInt)
 
-  override def getAmount: Double = ???
+  override def getAmount: Double = instance.getTotalAmount
 
-  override def setAmount(amount: Double): Unit = ???
+  override def setAmount(amount: Double): Unit = instance.setTotalAmount(amount)
+
+  def getMonthFromMonthDate(monthDate: String): String = monthDate.substring(0, 2)
+  def getYearFromMonthDate(monthDate: String): String = monthDate.substring(3, 5)
 }
