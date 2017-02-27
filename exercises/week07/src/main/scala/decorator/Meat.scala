bd2243454a097a722ac5394c6ba4dba936946095
Alexander Worton <kesomir@btinternet.com>
2017-02-27 16:33:57 +0000

    Added test and implemented decorator classes

---
+++ exercises/week07/src/main/scala/decorator/Meat.scala
@@ -0,0 +1,10 @@
+package decorator
+
+/**
+  * Created by aworton on 27/02/17.
+  */
+class Meat(pizza: Pizza) extends Pizza{
+  override def getDesc: Unit = ???
+
+  override def getPrice: Unit = ???
+}
---
+++ exercises/week07/src/main/scala/decorator/Pizza.scala
@@ -0,0 +1,9 @@
+package decorator
+
+/**
+  * Created by aworton on 27/02/17.
+  */
+trait Pizza {
+  def getDesc
+  def getPrice
+}
