d39b204111537276ea7ade7ded066ced3b4ff0d4
Alexander Worton <kesomir@btinternet.com>
2017-03-28 15:39:38 +0100

    interpreter question initial commit

---
+++ exercises/week10/src/main/scala/q03interpreter/Pints.scala
@@ -0,0 +1,13 @@
+package q03interpreter
+
+final case class Pints() extends Expression {
+  override def gallons(quantity: Double) = (quantity / 8).toString
+
+  override def quarts(quantity: Double)= (quantity / 2).toString
+
+  override def pints(quantity: Double) = quantity.toString
+
+  override def cups(quantity: Double) = (quantity * 2).toString
+
+  override def tablespoons(quantity: Double)= (quantity * 32).toString
+}
---
+++ exercises/week10/src/main/scala/q03interpreter/Quarts.scala
@@ -0,0 +1,13 @@
+package q03interpreter
+
+final case class Quarts() extends Expression {
+  override def gallons(quantity: Double) = (quantity / 4).toString
+
+  override def quarts(quantity: Double) = (quantity).toString
+
+  override def pints(quantity: Double) = (quantity * 2).toString
+
+  override def cups(quantity: Double) = (quantity * 4).toString
+
+  override def tablespoons(quantity: Double): String = (quantity * 64).toString
+}
