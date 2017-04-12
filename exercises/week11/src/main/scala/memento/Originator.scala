dba34ea363ee5206f2313e347c1ec706425bf542
Alexander Worton <kesomir@btinternet.com>
2017-04-12 16:33:20 +0100

    Memento implemented

--- exercises/week11/src/main/scala/memento/Originator.scala
+++ exercises/week11/src/main/scala/memento/Originator.scala
@@ -10,13 +10,31 @@ case class Originator(
 
   createSavepoint("INITIAL")
 
-  def createSavepoint(savepointName: String): Unit = ???
+  def createSavepoint(savepointName: String): Unit = {
+    println(s"Saving state...$savepointName")
+    careTaker.saveMemento(Memento(this.x, this.y), savepointName)
+    lastUndoSavepoint = savepointName
+  }
 
-  def undo(): Unit = ???
+  def undo(): Unit = {
+    println(s"Undo at...$lastUndoSavepoint")
+    val rollback = careTaker.getMemento(lastUndoSavepoint)
+    this.x = rollback.x
+    this.y = rollback.y
+  }
 
-  def undo(savepointName: String): Unit = ???
+  def undo(savepointName: String): Unit = {
+    println(s"Undo at...$savepointName")
+    val rollback = careTaker.getMemento(savepointName)
+    this.x = rollback.x
+    this.y = rollback.y
+  }
 
-  def undoAll(): Unit = ???
+  def undoAll(): Unit = {
+    println("Clearing all save points...")
+    undo("INITIAL")
+    careTaker.clearSavepoints()
+  }
 
   private def setOriginatorState(savepointName: String): Unit = ???
 
